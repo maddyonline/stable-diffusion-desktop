97d1926d82a2fa8b65c5e6c3614011a3c26948ac
Madhav Jha <madhavjha@gmail.com>
2022-09-03 16:06:56 -0700

    initial react+tailwind setup

---
+++ app/src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+import "./index.css";
+
+const container = document.getElementById("target");
+const root = createRoot(container);
+
+root.render(
+  <React.Fragment>
+    <App />
+  </React.Fragment>
+);
